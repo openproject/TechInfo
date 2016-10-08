26de8cbe28832abdf74c4ec9eaf7defc8f0d458c
fengjian <fengjian@lez.cn>
2016-10-08 18:17:23 +0800

    fix(text): update some text info: github link, readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Tech Info Open Platform
+包括爬虫、数据库、网站三部分
--- web/index.php
+++ web/index.php
@@ -131,7 +131,7 @@
                     <div class="weui-footer">
                         <p class="weui-footer__links">
                             <a href="http://www.jayfeng.com" class="weui-footer__link">杰风居出品</a>
-                            <a href="javascript:void(0);" class="weui-footer__link">Github</a>
+                            <a href="https://github.com/openproject/TechInfo" class="weui-footer__link">Github</a>
                         </p>
                         <p class="weui-footer__text">Copyright &copy; 20015-2016 jayfeng.com</p>
                     </div>
