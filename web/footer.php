9022d050e4fb7da4a5a3ef0d2b0fa73e172b4128
fengjian <673592063@qq.com>
2016-10-11 11:01:06 +0800

    refactor(footer): refactor the common footer php to footer.php and clean some code

---
+++ web/footer.php
@@ -0,0 +1,11 @@
+<br />
+<br />
+<br />
+<div class="weui-footer">
+	<p class="weui-footer__links">
+		<a href="https://github.com/openproject/TechInfo" class="weui-footer__link">搜索</a>
+		<a href="http://www.jayfeng.com" class="weui-footer__link">杰风居出品</a>
+		<a href="https://github.com/openproject/TechInfo" class="weui-footer__link">Github</a>
+	</p>
+	<p class="weui-footer__text">Copyright &copy; 20015-2016 jayfeng.com</p>
+</div>
--- web/query.php
+++ web/query.php
@@ -64,19 +64,10 @@
 					<div class="weui-panel__bd" id="search_result_list">
 				</div>
 			</div>
-
-			<br />
-			<br />
-			<br />
-			<div class="weui-footer">
-				<p class="weui-footer__links">
-					<a href="https://github.com/openproject/TechInfo" class="weui-footer__link">搜索</a>
-					<a href="http://www.jayfeng.com" class="weui-footer__link">杰风居出品</a>
-					<a href="https://github.com/openproject/TechInfo" class="weui-footer__link">Github</a>
-				</p>
-				<p class="weui-footer__text">Copyright &copy; 20015-2016 jayfeng.com</p>
-			</div>
-
+			
+			<?php
+				include("footer.php");
+			?>
 <script type="text/javascript">
 
     $(function(){
